f9414865ac51e15fb78f229504823ccf37318add
Elmehdi HAYTOM <67712394+ElmehdiHAYTOM@users.noreply.github.com>
2024-03-29 20:51:31 +0000

    Update voicecreate.py
    bot.start() instead of bot.run()

--- voicecreate.py
+++ voicecreate.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import traceback
 import os
 import sys
+import asyncio
 
 intents=discord.Intents.all()
 bot = commands.Bot(command_prefix=".",intents=intents)
@@ -12,14 +13,18 @@ bot.remove_command("help")
 
 initial_extensions = ['cogs.voice',]
 
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)
-            print(extension,"loaded")
-        except Exception as e:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
-            traceback.print_exc()
+
+async def main():
+    async with bot:
+        for extension in initial_extensions:
+            try:
+                await bot.load_extension(extension)
+                print(extension,"loaded")
+            except Exception as e:
+                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                traceback.print_exc()
+        await bot.start(os.environ["BOT_TOkEN"])
+
 
 @bot.event
 async def on_ready():
@@ -27,4 +32,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-bot.run(os.environ["BOT_TOkEN"])
+
+if __name__ == '__main__':
+    asyncio.run(main())
